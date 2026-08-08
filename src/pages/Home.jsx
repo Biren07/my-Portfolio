5548304b855289c025764fe7531ee780dda8a131
[firstname lastname] <dhamib610@gmail.com>
2026-08-08 10:06:02 +0545

    Update me

---
+++ src/pages/Home.jsx
@@ -0,0 +1,27 @@
+import { Navbar } from "../components/Navbar";
+import { HeroSection } from "../components/HeroSection";
+import { AboutSection } from "../components/AboutSection";
+import { SkillsSection } from "../components/SkillsSection";
+import { ProjectsSection } from "../components/ProjectsSection";
+import { CertificatesSection } from "../components/CertificatesSection";
+import { ContactSection } from "../components/ContactSection";
+import { Footer } from "../components/Footer";
+
+export const Home = () => {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      <Navbar />
+      <main>
+        <HeroSection />
+        <AboutSection />
+        <SkillsSection />
+        <ProjectsSection />
+        <CertificatesSection />
+        <ContactSection />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
